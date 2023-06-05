8bd7ae3c8bd60256adf68939da2f375db812999b
RFauzi <119996191+rfauzi44@users.noreply.github.com>
2023-06-05 23:53:31 +0700

    [FIX] React DOM depreciated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -10,16 +10,16 @@ import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 
 let persistor = persistStore(store);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
